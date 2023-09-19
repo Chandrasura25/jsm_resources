122c58ab667b5979859a4bb015c53338575bfd6b
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-19 16:59:34 +0100

    search form

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -19,6 +19,8 @@ const SearchForm = () => {
           className="base-regular h-filt border-0 bg-black-400 py-6 pl-20 pr-8 text-white-800 !ring-0 !ring-offset-0 placeholder:text-white-800"
           type="text"
           placeholder="Search"
+          value={search}
+          onChange={(e)=> setSearch(e.target.value)}
         />
       </label>
     </form>
