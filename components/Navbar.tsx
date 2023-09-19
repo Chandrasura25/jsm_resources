127e63323cecfd24f68bc2d241590178bdf8b462
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-19 15:53:32 +0100

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-7 text-white'>
+        <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16"></div>
+    </nav>
   )
 }
 
