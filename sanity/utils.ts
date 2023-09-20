063c35d746222a8eb46c92884efc0b482550fd24
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-20 11:31:18 +0100

    utils

--- sanity/utils.ts
+++ sanity/utils.ts
@@ -10,4 +10,6 @@ export function buildQuery(params: BuildQueryParams) {
   const { type, query, category, page = 1, perPage = 10 } = params;
   const conditions = [`*[_type=="${type}"]`];
   if (query) conditions.push(`title match "*${query}*"`);
+  if (category && category !== "all")
+    conditions.push(`category == "${category}"`);
 }
