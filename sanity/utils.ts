ad25f3c450e9b31d5a040f5197f4a50a7f127610
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-20 11:34:38 +0100

    errror

--- sanity/utils.ts
+++ sanity/utils.ts
@@ -2,14 +2,23 @@ interface BuildQueryParams {
   type: string;
   query: string;
   category: string;
-  page: string;
-  perPage?: string;
+  page: number;
+  perPage?: number;
 }
 
 export function buildQuery(params: BuildQueryParams) {
   const { type, query, category, page = 1, perPage = 10 } = params;
+
   const conditions = [`*[_type=="${type}"]`];
-  if (query) conditions.push(`title match "*${query}*"`);
-  if (category && category !== "all")
+
+  if (query) {
+    conditions.push(`title match "*${query}*"`);
+  }
+
+  if (category && category !== "all") {
     conditions.push(`category == "${category}"`);
+  }
+  //calculate pagination limits
+  const offset = (page - 1) * perPage;
+  const limit = perPage;
 }
