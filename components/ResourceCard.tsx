746e15784486271622f408e43fb7fcc5792a7c88
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-20 12:09:23 +0100

    card

--- components/ResourceCard.tsx
+++ components/ResourceCard.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Card, CardHeader } from "./ui/card";
+import Link from "next/link";
+import Image from "next/image";
 interface Props {
   id: string;
   title: string;
@@ -7,7 +10,23 @@ interface Props {
   slug: string;
 }
 const ResourceCard = ({ id, title, image, downloadNumber, slug }: Props) => {
-  return <div>ResourceCard</div>;
+  return (
+    <Card className="w-full max-w-filt border-0 !bg-transparent sm:max-w-[356px]">
+      <Link href={`/resource/${id}`}>
+        <CardHeader>
+          <div className="">
+            <Image
+              src={image}
+              alt={title}
+              className="h-full rounde-md object-cover"
+              height={384}
+              width={440}
+            />
+          </div>
+        </CardHeader>
+      </Link>
+    </Card>
+  );
 };
 
 export default ResourceCard;
