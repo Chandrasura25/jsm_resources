5afb226aaad2d93eb29edae6064f49cfe3f27204
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-21 11:54:00 +0100

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,8 +5,22 @@ interface Props{
     category: string
 }
 const Header = ({title, query, category}:Props) => {
+    if(query && category){
+        return(
+            <h1 className="heading3 self-start text-white-800">
+                Search results for "{query}" in <span className="capitalize">{category}</span> 
+            </h1>
+        )
+    }
+    if(query){
+        return(
+            <h1 className="heading3 self-start text-white-800">
+                Search results for "{query}"
+            </h1>
+        )
+    }
   return (
-    <div>Header</div>
+    <h1 className="heading3 self-start text-white-800">No Results</h1>
   )
 }
 
