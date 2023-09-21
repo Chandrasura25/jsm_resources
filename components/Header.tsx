2d447980634f30b3b6df91fedd348cc29f9d6697
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-21 11:56:28 +0100

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,27 +1,33 @@
-import React from 'react'
-interface Props{
-    title: string;
-    query:string;
-    category: string
+import React from "react";
+interface Props {
+  title: string;
+  query: string;
+  category: string;
 }
-const Header = ({title, query, category}:Props) => {
-    if(query && category){
-        return(
-            <h1 className="heading3 self-start text-white-800">
-                Search results for "{query}" in <span className="capitalize">{category}</span> 
-            </h1>
-        )
-    }
-    if(query){
-        return(
-            <h1 className="heading3 self-start text-white-800">
-                Search results for "{query}"
-            </h1>
-        )
-    }
-  return (
-    <h1 className="heading3 self-start text-white-800">No Results</h1>
-  )
-}
+const Header = ({ title, query, category }: Props) => {
+  if (query && category) {
+    return (
+      <h1 className="heading3 self-start text-white-800">
+        Search results for "{query}" in{" "}
+        <span className="capitalize">{category}</span>
+      </h1>
+    );
+  }
+  if (query) {
+    return (
+      <h1 className="heading3 self-start text-white-800">
+        Search results for "{query}"
+      </h1>
+    );
+  }
+  if (category) {
+    return (
+      <h1 className="heading3 self-start text-white-800">
+        <span className="capitalize">{category}</span>
+      </h1>
+    );
+  }
+  return <h1 className="heading3 self-start text-white-800">No Results</h1>;
+};
 
-export default Header
+export default Header;
