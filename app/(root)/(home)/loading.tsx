3dece3efd21b64d59bbc6ef674f8df8dca426c99
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-22 19:18:25 +0100

    done

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -4,14 +4,14 @@ const Loading = () => {
   return (
     <main className="flex-center paddings mx-auto flex-col">
       <section className="nav-paddings w-full">
-        <Skeleton className="h-[274px] w-full rounded-lg" />
+        <Skeleton className="h-[274px] w-full rounded-lg bg-gray-800"  />
       </section>
       <section className="mt-6 flex w-full flex-col sm:mt-20">
-        <Skeleton className="h-10 w-56" />
+        <Skeleton className="h-10 w-56 bg-gray-800" />
         <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
-          <Skeleton className="h-[440px] w-full sm:w-[356px]" />
-          <Skeleton className="h-[440px] w-full sm:w-[356px]" />
-          <Skeleton className="h-[440px] w-full sm:w-[356px]" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-gray-800" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-gray-800" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-gray-800" />
         </div>
       </section>
     </main>
