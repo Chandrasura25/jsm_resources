e1558e2518df058ef59c65b77ec3ce4fad63e3ae
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-22 19:10:52 +0100

    skeleton

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import { Skeleton } from "@/components/ui/skeleton"
 
 const Loading = () => {
   return (
-    <div>Loading</div>
+    <main className="flex-center paddings mx-auto flex-col">
+      <section className="nav-paddings w-full">
+      <Skeleton className="h-[274px] w-full rounded-lg" />
+      </section>
+    </main>
   )
 }
 
