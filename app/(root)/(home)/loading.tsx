33625defd6e13812d567b6507e24ea1bcdc11193
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-22 19:11:54 +0100

    skeleton

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -1,13 +1,16 @@
-import { Skeleton } from "@/components/ui/skeleton"
+import { Skeleton } from "@/components/ui/skeleton";
 
 const Loading = () => {
   return (
     <main className="flex-center paddings mx-auto flex-col">
       <section className="nav-paddings w-full">
-      <Skeleton className="h-[274px] w-full rounded-lg" />
+        <Skeleton className="h-[274px] w-full rounded-lg" />
+      </section>
+      <section className="mt-6 flex w-full flex-col sm:mt-20">
+        <Skeleton className="h-10 w-56" />
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;
