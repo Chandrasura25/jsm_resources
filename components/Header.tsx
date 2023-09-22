c5a5392a6cf3a4247a7f2132d1086dc49e8a0901
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-22 19:17:55 +0100

    done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 interface Props {
-  title: string;
   query: string;
   category: string;
 }
-const Header = ({ title, query, category }: Props) => {
+const Header = ({query, category }: Props) => {
   if (query && category) {
     return (
       <h1 className="heading3 self-start text-white-800">
