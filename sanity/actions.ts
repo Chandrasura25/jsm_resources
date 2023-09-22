6cd686dd713faf737c7a478607f8413fa146e735
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-22 18:22:37 +0100

    action

--- sanity/actions.ts
+++ sanity/actions.ts
@@ -30,3 +30,25 @@ export const getResources = async (params: GetResourcesParams) => {
     console.log(error);
   }
 };
+export const getResourcesPlaylist = async (params: GetResourcesParams) => {
+  const { query, category, page } = params;
+  try {
+    const resources = await readClient.fetch(
+      groq`*[_type == "resourcePlaylist"]{
+        _id,
+        title,
+        respirces[0...6]->{
+          title,
+          _id,
+        downloadLink,
+        "image":poster.asset->url,
+        views,
+        category
+        }
+      }`
+    );
+    return resources;
+  } catch (error) {
+    console.log(error);
+  }
+};
