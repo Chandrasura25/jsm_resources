49e4e2d66195712c2a41975848b0895637c23f57
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-22 19:13:52 +0100

    skeleton

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -8,6 +8,11 @@ const Loading = () => {
       </section>
       <section className="mt-6 flex w-full flex-col sm:mt-20">
         <Skeleton className="h-10 w-56" />
+        <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
+          <Skeleton className="h-[440px] w-full sm:w-[356px]" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px]" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px]" />
+        </div>
       </section>
     </main>
   );
